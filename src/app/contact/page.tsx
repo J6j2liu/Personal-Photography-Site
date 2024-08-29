23942952c8c4098ba632018031cf03ed88837648
J6j2liu <78995199+J6j2liu@users.noreply.github.com>
2024-08-29 15:52:20 -0700

    Link updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 
           <div className="relative z-30 text-3xl font-bold py-10">Project Repository</div>
           <div className="flex relative max-w-[900px] justify-center justify-between gap-5 cursor-pointer">
-            <Link href="https://github.com/J6j2liu/website" target="_blank">
+            <Link href="https://github.com/J6j2liu/Personal-Photography-Site" target="_blank">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
