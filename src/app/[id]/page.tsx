ffc061352e3de7f04fa93e41ef33b3e34df76549
J6j2liu <78995199+J6j2liu@users.noreply.github.com>
2024-09-06 19:19:26 -0700

    error and margin fix

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -18,42 +18,51 @@ export default function Page({ params }: { params: { id: string } }) {
     const [open, setOpen] = React.useState(false);
     const [ind, setInd] = React.useState(0);
 
-    return (
-        <div className="h-full overflow-auto">
-            <main className="relative pt-[110px] z-20">
-                <div className="text-center uppercase text-xl select-none">{dic[params.id].title}</div>
+    if (dic[params.id] == undefined) {
+        return (
+            <div className="h-full overflow-auto">
+                <main className="relative pt-[110px] z-20 text-center text-4xl justify-center">Page Not Found :(</main>
+            </div>
+        );
+    } else {
 
-                <div className="flex flex-col h-full items-center">
-                    <Masonry breakpointCols={2} className="flex gap-4 h-full max-w-[900px] w-full p-2 sm:p-4 my-6 select-none">
-                        {dic[params.id].data.map((image, idx) => (
+        return (
+            <div className="h-full overflow-auto">
+                <main className="relative pt-[110px] z-20">
+                    <div className="text-center uppercase text-xl select-none p2">{dic[params.id].title}</div>
 
-                            <Image
-                                key={image.src}
-                                src={image}
-                                alt="reb1"
-                                className="my-4 cursor-pointer"
-                                placeholder="blur"
-                                onClick={() => { setOpen(true), setInd(idx) }}
-                            />
-                        )
-                        )}
-                    </Masonry>
+                    <div className="flex flex-col h-full items-center">
+                        <Masonry breakpointCols={2} className="flex gap-4 h-full max-w-[900px] w-full p-2 sm:p-4 my-6 select-none">
+                            {dic[params.id].data.map((image, idx) => (
 
-                    <Lightbox
-                        open={open}
-                        close={() => setOpen(false)}
-                        slides={dic[params.id].data}
-                        index={ind}
-                        plugins={[Zoom, Thumbnails]}
-                    />
-                </div>
-            </main>
+                                <Image
+                                    key={image.src}
+                                    src={image}
+                                    alt="reb1"
+                                    className="my-4 cursor-pointer"
+                                    placeholder="blur"
+                                    onClick={() => { setOpen(true), setInd(idx) }}
+                                />
+                            )
+                            )}
+                        </Masonry>
 
-            <footer className="relative h-[90px] flex justify-center align-center uppercase text-lg font-medium z-20 select-none">
-                <p>Jonathan Liu Photography</p>
-            </footer>
-        </div>
+                        <Lightbox
+                            open={open}
+                            close={() => setOpen(false)}
+                            slides={dic[params.id].data}
+                            index={ind}
+                            plugins={[Zoom, Thumbnails]}
+                        />
+                    </div>
+                </main>
 
-    );
+                <footer className="relative h-[90px] flex justify-center align-center uppercase text-lg font-medium z-20 select-none">
+                    <p>Jonathan Liu Photography</p>
+                </footer>
+            </div>
+
+        );
+    }
 }
 
--- src/app/_images.tsx
+++ src/app/_images.tsx
@@ -179,3 +179,4 @@ export const pics: string[] =
         "06",
         "07",
     ];
+
